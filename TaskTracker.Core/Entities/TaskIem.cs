2ece3bdfa894fc9a92ad875e4b03035f270d02af
randall Fine <randallfine@gmail.com>
2025-04-22 17:11:30 -0500

    updated taskitem entitiy, added enum folder and Priority enum, added Interfaces folder and ITaskRepositor

--- TaskTracker.Core/Entities/TaskIem.cs
+++ TaskTracker.Core/Entities/TaskIem.cs
@@ -1,10 +1,11 @@
 ﻿namespace TaskTracker.Core.Entities;
 
-public class TaskIem
+public class TaskItem
 {
     public int Id { get; set; }
     public string Title { get; set; }
-    public string Description { get; set; }
-    public DateTime DueDate { get; set; }
+    public string? Description { get; set; }
+    public DateTime? DueDate { get; set; }
     public bool IsCompleted { get; set; }
+    public Priority Priority { get; set; } = Priority.Medium;
 }
---
+++ TaskTracker.Core/Interfaces/ITaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using TaskTracker.Core.Entities;
+namespace TaskTracker.Core.Interfaces
+;
+
+public interface ITaskRepository
+{
+    Task<List<TaskItem>> GetTasksAsync();
+    Task<TaskItem> GetTaskAsync(int taskId);
+    Task<TaskItem> AddTaskAsync(TaskItem task);
+    Task<TaskItem> UpdateTaskAsync(TaskItem task);
+    Task<TaskItem> DeleteTaskAsync(int taskId);
+}
